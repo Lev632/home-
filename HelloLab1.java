ef98bfee2440a20317d8afcfd2a7cae2ec5068d4
Lev632 <114541520+Lev632@users.noreply.github.com>
2022-09-27 19:38:04 -0500

    Create HelloLab1.java

---
+++ HelloLab1.java
@@ -0,0 +1,12 @@
+// HelloLab1.java 
+
+public class HelloLab1 {
+
+  Public static void main(string[] args) {
+    int sum = 123 + 456; 
+    System.out.println("Hello Lab 1");
+    System.out.println("The sum of 123 and 456 is: " + sum);
+  }
+
+
+}
